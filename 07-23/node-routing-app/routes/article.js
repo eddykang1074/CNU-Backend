2e3dc9ceea6d2efd1822e86978182c3e764a6c22
eddy <ceo@msoftware.co.kr>
2024-07-23 16:50:22 +0900

    feat: 게시글 수정/삭제/목록이동 코드 추가

--- 07-23/node-routing-app/routes/article.js
+++ 07-23/node-routing-app/routes/article.js
@@ -81,12 +81,99 @@ router.post('/create',function(req,res){
 });
 
 
+//게시글 삭제처리 요청과 응답처리 라우팅메소드
+//요청주소: http://localhost:3000/article/delete
+//클라이언트 요청방식: Post 방식
+//응답결과: 삭제후 목록페이지 이동처리 
+router.post('/delete',function(){
+
+    //Step1: 삭제할 게시글 고유번호 추출하기 
+    const articleIdx = req.body.article_id;
+
+    //Step2: db 게시글테이블에서 해당 게시글번호로 단일게시글 정보를 영구 삭제한다.
+
+    //Step3: 삭제처리후 목록 페이지로 이동시키기 
+    res.redirect('/article/list');
+
+    
+});
+
+
+
+
 //게시글 확인 및 수정 웹페이지 요청과 응답처리 라우팅메소드
-//요청주소: http://localhost:3000/article/modify
+//요청주소: http://localhost:3000/article/modify?id=1
 //클라이언트 요청방식: Get방식
 //응답형식: 단일 게시글 정보 확인 웹페이지(뷰파일)
 router.get('/modify',function(req,res){
-    res.render('article/modify.ejs');
+
+    //URL 주소를 통해 데이터를 전달하는 방법 2가지 
+    //1)QueryString방식 : url주소에 ?키1=값&키2=값&키3=값
+    //QueryString방식 예시: http://shop.naver.com/category?ptype=tv&manufacture=lg&price=5000
+    //http://test.co.kr/blogs?id=1
+
+    //2)파라메터 방식 : url주소내에 데이터를 포함시키는 방식 
+    //파라메터 방식 예시: http://test.co.kr/blogs/1  http://test.co.kr/category/1/goods/2000
+
+    //Step1:URL주소에서 게시글 고유번호를 추출한다.
+    //쿼리스트링방식으로 전달되는 키값은 req.query.키명으로 추출한다. 
+    const articleIdx = req.query.id;
+
+    //Step2: 해당 게시글 번호를 이용해 DB 게시글 테이블에서 단일게시글 정보를 조회해온다.
+    //언제?? 나중에..
+    //예시아래 데이터를 db에서 불라왔다는 가정하에..
+    const article = {
+        article_id:1,
+        title:"웹퍼블리셔의 업무에 대해 궁금해요.",
+        contents:"웹퍼블리셔의 주요업무 2가지 웹표준준수코딩,웹접근성준수,반응형웹페이지구현...블라블라..",
+        display:1,
+        view_cnt:100,
+        regist_date:"2024-07-23",
+        regist_id:1,
+    }
+
+    //지정된 뷰파일에 단일게시글 데이터를 article이라는 속성명으로 전달한다. 
+    res.render('article/modify.ejs',{article});
+    //객체의 속성명과 속성명의 값변수값이 동일하면 변수명은 생략..
+    //res.render('article/modify.ejs',{article:article});
+});
+
+
+
+//*** 라우팅 메소드 구현시 가장 중요한점 : 와일드카드로 구현된 라우팅메소드는 모든 라우팅메소드의 최하단 위치에 구현할것.. */
+//기존 게시글 정보에대해 사용자가 수정한 폼정보를 이용해 
+//수정데이터를 폼에서 추출하고 추출한 수정정보를 기반으로
+//DB에 저장되어 있던 기존 데이터를 수정처리후에 목록페이지 이동시킬까? 말까?? 결정은 여러분이 하는것입니다.
+//관리자 웹사이트의 특성상 목록페이지로 그냥 이동시켜버림..
+//요청주소: http://localhost:3000/article/modify/1
+//클라이언트 요청방식: POST방식
+//응답형식: 웹브라우저 주소를 목록페이지로 이동시킴..res.redirect('이동시킬url주소')
+router.post('/modify/:id',function(req,res){
+
+    //URL파라메터 방식으로 데이터를 전달하는경우 해당 데이터를 URL에서 추출하는 방법
+    //먼저 라우팅 주소에 와일드카드 키를 설정한다  /modify/:id   :id가 와일드카드 키명
+
+    //Step1: 게시글 고유번호를 추출한다. :와일드카드 키명으로 파라메터 값 추출하기
+    //파라메터방식으로 url을 통해데이터르 전달하는경우 req.params.와일드카드키명으로 값을 추출한다. 
+    const articleIdx = req.params.id; 
+
+    //Step2: 사용자가 수정한 HTML요소의 수정값 추출하기 
+    const title = req.body.title;
+    const contents = req.body.contents;
+    const display = req.body.display;
+
+    //Step3: db 게시글 정보 수정을 위한 json 수정데이터 생성하기 
+    const article = {
+        title:title,
+        contents:contents,
+        display:display,
+    }
+
+    //Step4: db에 해당 단일 게시글에 대해 상기 수정데이터로 데이터를 수정처리한다. 
+    
+    //수정작업이 끝나면 목록페이지로 이동시키거나 특정뷰파일을 보내준다(res.render(...))
+    res.redirect('/article/list');
+
 });
 
 
