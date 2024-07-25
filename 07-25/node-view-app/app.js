ac068709a6c0433dfada5e66f0f934ca0630bd15
eddy <ceo@msoftware.co.kr>
2024-07-25 17:46:26 +0900

    new: node-view-app 프로젝트 추가

---
+++ 07-25/node-view-app/app.js
@@ -0,0 +1,51 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+//개발자 정의 게시글 웹페이지 요청과 응답라우터 파일 참조하기 
+var articleRouter = require('./routes/article.js');
+
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+//articleRouter 라우터 파일의 기본 URL주소 체계 정의합니다.
+//localhost:3000/article
+app.use('/article', articleRouter);
+
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ 07-25/node-view-app/routes/article.js
@@ -0,0 +1,155 @@
+//article.js 라우터파일은 
+//게시글관련 각종 웹페이지들에 대한 요청과 응답을 처리한다.
+//본 라우터파일의 기본호출주소: http://localhost:3000/article 로시작하게 
+//app.js에서 라우터 파일 참조시 기본주소를 설정해준다.
+var express = require('express');
+var router = express.Router();
+
+
+/* 
+- 게시글 목록 웹페이지 요청과 응답처리 라우팅 메소드 구현
+- 호출주소: http://localhost:3000/article/list
+- 호출방식: Get
+- 응답결과: 게시글 목록데이터를 기반으로한 게시글 목록 웹페이지 전달 
+*/
+router.get('/list',async(req,res)=>{
+
+    //게시글 데이터 목록 3개 생성-추후 DB에서 데이터를 가져옵니다.
+    const articles = [
+        {
+            article_id:"1",
+            title:"게시글 제목1입니다.",
+            contents:"게시글1 내용입니다.",
+            display:1,
+            view_cnt:10,
+            ip_address:"111.111.111.111",
+            regist_id:1,
+            regist_date:Date.now()
+        },
+        {
+            article_id:2,
+            title:"게시글 제목2입니다.",
+            contents:"게시글2 내용입니다.",
+            display:0,
+            view_cnt:11,
+            ip_address:"222.111.111.111",
+            regist_id:2,
+            regist_date:Date.now()
+        },
+        {
+            article_id:3,
+            title:"게시글 제목3입니다.",
+            contents:"게시글3 내용입니다.",
+            display:1,
+            view_cnt:30,
+            ip_address:"211.111.111.111",
+            regist_id:3,
+            regist_date:Date.now()
+        }
+    ];
+
+
+
+    //물리적경로: views/article/list.ejs
+    res.render('article/list.ejs',{articles:articles});
+});
+
+
+/* 
+- 신규 게시글 등록 웹페이지 요청과 응답처리 라우팅 메소드 구현
+- 호출주소: http://localhost:3000/article/create
+- 호출방식: Get
+- 응답결과: 게시글 등록 웹페이지 뷰파일 전달
+*/
+router.get('/create',async(req,res)=>{
+    res.render('article/create.ejs');
+});
+
+
+
+/* 
+- 신규 게시글 등록 웹페이지에서 보내준 
+- 사용자가 입력/선택한 신규 게시글 데이터를 등록 처리 요청과 응답처리 라우팅 메소드 구현
+- 호출주소: http://localhost:3000/article/create
+- 호출방식: Post
+- 응답결과: 신규 게시글 DB등록 처리후 특정 페이지로 이동 또는 특정뷰파일 제공
+** 라우팅 주소와 요청방식 2가지 동일해야 해당 라우팅 메소드가 호출되고 실행된다.
+*/
+router.post('/create',async(req,res)=>{
+
+    //Step1: 사용자 입력한 폼태그내 입력/선택 데이터 추출하기 
+    //req.body.전달된 폼태그내 HTML 입력/선택 요소의 name속성명으로 지정
+    const title = req.body.title;
+    const contents = req.body.contents;
+    const display = req.body.display;
+
+    //Step2: DB 게시글 테이블에 저장할 JSON 데이터 생성하기
+    //객체 속성명과 속성의 데이터값 변수/상수명이 같으면 상수/변수명은 생략가능하다.
+    const article = {
+        title,
+        contents:contents,
+        display,
+        ip_address:"111.111.111.111",
+        view_cnt:0,
+        regist_id:1,
+        regist_date:Date.now()
+    };
+
+    //Step3:DB 게시글 테이블에 상기 article 데이터 등록처리(Insert Into Table명...)
+    //DB서버에서 Insert SQL구문을 통해서 DB등록처리가 되면 등록된 실제 데이터셋을 다시 반환함
+    const registedArticle = {
+        article_id:1,
+        title,
+        contents:contents,
+        display,
+        ip_address:"111.111.111.111",
+        view_cnt:0,
+        regist_id:1,
+        regist_date:Date.now()
+    };
+
+    //Step4: 등록완료 후 게시글 목록 페이지로 이동시킴
+    //http://localhost:3000/article/list
+    res.redirect('/article/list');
+});
+
+
+/*
+-기존 등록된 게시글 데이터를 조회해서 게시글 수정 웹페이지에 데이터를 포함한 웹페이지 요청과 응답처리 라우팅메소드 
+-호출주소: http://localhost:2000/article/modify/1
+-호출방식: Get
+-응답결과: db에서 해당 단일게시글 정보를 조회해와서 지정 뷰파일에 데이터를 전달하고 뷰파일내에서 해당 데이터를 html태그에 출력해서 최종 
+-웹브라우저에 동적으로 변경된 웹페이지를 반환한다. 
+*/
+router.get('/modify/:idx',async(req,res)=>{
+
+    //Step1: url주소에서 게시글 고유번호를 추출합니다.
+    const articleIdx = req.params.idx;
+
+    //Step2: DB 게시글 테이블에서 해당 게시글 고유번호에 해당하는 단일게시글 정보를 조회해옵니다.
+    //조회해 왔다고 가정합니다.
+    const article = {
+        article_id:1,
+        title:"게시글 제목1입니다.",
+        contents:"게시글1 내용입니다.",
+        display:1,
+        view_cnt:10,
+        ip_address:"111.111.111.111",
+        regist_id:1,
+        regist_date:Date.now()
+    }
+
+    //Step3: db에서 가져온 단일게시글 정보를 modify.ejs 뷰파일에 전달합니다. 
+    res.render('article/modify.ejs',{article});
+});
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
