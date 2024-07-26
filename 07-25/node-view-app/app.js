0d3e141b75d9f1ae9e2c112f40cdbba1bb0b4b9b
eddy <ceo@msoftware.co.kr>
2024-07-26 13:12:41 +0900

    feat: 화면 재활용기술 실습-인클루드/레이아웃 기술

--- 07-25/node-view-app/app.js
+++ 07-25/node-view-app/app.js
@@ -4,6 +4,10 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+//레이아웃 노드패키지 참조하기 
+var expressLayouts = require('express-ejs-layouts');
+
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -16,6 +20,14 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+//모든 뷰파일에 적용되는 레이아웃 뷰파일 설정하기 
+app.set('layout', 'layout.ejs'); //전체레이아웃 파일 지정하기
+app.set("layout extractScripts", true);
+app.set("layout extractStyles", true);
+app.set("layout extractMetas", true); 
+app.use(expressLayouts); //노드앱에 레이아웃 기능 추가적용ㅇ하기 
+
 
 app.use(logger('dev'));
 app.use(express.json());
