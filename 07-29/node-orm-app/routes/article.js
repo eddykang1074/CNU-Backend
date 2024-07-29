4ef27193b414400ac2c9610ecbf50a88cb6c5a05
eddy <ceo@msoftware.co.kr>
2024-07-29 18:14:48 +0900

    게시글 전체 /등록 기능추가

--- 07-29/node-orm-app/routes/article.js
+++ 07-29/node-orm-app/routes/article.js
@@ -4,12 +4,22 @@
 var express = require('express');
 var router = express.Router();
 
+
+//DB프로그래밍을 위한 ORM DB객체 참조하기
+var db = require('../models/index');
+
+
+
 //게시글 전체목록조회 웹페이지 요청과 응답처리 라우팅메소드
 //호출주소: http://localhost:3000/article/list
 //호출방식: Get
 //응답결과: 전체 게시글 목록이 포함된 웹페이지 반환
 router.get('/list',async(req,res)=>{
-    res.render('article/list.ejs');
+    
+    //전체 게시글 목록 조회하기
+    const articles = await db.Article.findAll();
+
+    res.render('article/list.ejs',{articles});
 });
 
 //신규 게시글 등록 웹페이지 요청과 응답처리 라우팅메소드
@@ -21,6 +31,34 @@ router.get('/create',async(req,res)=>{
 //신규 게시글 입력정보 등록처리 요청과 응답처리 라우팅메소드
 //호출주소: http://localhost:3000/article/create
 router.post('/create',async(req,res)=>{
+
+    //Step1: 신규 게시글 등록폼에서 사용자가 입력/선택한 값을 추출하자.
+    const title = req.body.title;
+    const contents = req.body.contents;
+    const display_code = req.body.display;
+
+    //Step2: article 테이블에 등록할 json 데이터 생성하기
+    //주의/중요: 반드시 json 데이터 속성명은 article.js모델의 속성명과 일치해야한다.
+    const article = {
+        board_type_code:1,
+        title:title,
+        article_type_code:0,
+        contents:contents,
+        view_count:0,
+        ip_address:'123.111.111.111',
+        is_display_code:display_code,
+        reg_date:Date.now(),
+        reg_member_id:1
+    };
+
+    //Step3: 준비된 신규 게시글 데이터를 article테이블에 저장한다.
+    //create()메소드는 ORM Framework의해 INSERT INTO article()values()쿼리로 변환되어
+    //DB서버에 전송되어 DB서버에서 실행되고 실제 저장된 단일게시글 DATA를 DB서버에서 반환한다.
+    const registedArticle = await db.Article.create(article);
+    console.log("실제 DB article 테이블에 저장된 데이터확인:",registedArticle);
+
+
+
    //신규게시글 db등록처리후
    //목록 페이지로 이동
    res.redirect('/article/list');
