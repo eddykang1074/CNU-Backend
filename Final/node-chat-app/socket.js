71093d10a85064400d91ba87edd41a4b2fa8b751
eddy <ceo@msoftware.co.kr>
2024-07-31 14:30:46 +0900

    서버소켓 구현

--- Final/node-chat-app/socket.js
+++ Final/node-chat-app/socket.js
@@ -33,8 +33,42 @@ module.exports =(server)=>{
         });
 
 
+        //지정한 채팅방 개설 및 입장처리 메시지 이벤트 수신기 
+        //socket.on('서버측이벤트 수신기명',콜백함수(){});
+        socket.on('entry',async(channel,nickName)=>{
+            //socket.join('채팅방이름');
+            //socket.join('채팅방이름')은 서버환경에 해당 채팅방이름으로
+            //채널을 개설하고 현재 입장하는 사용자를 해당 채팅방 사용자로 등록처리한다.
+            //이미 해당 채널이 개설되어 있으면 신규개설하지 않고 기존 채널로 입장한다.
+            socket.join(channel);
+
+            //현재 접속자를 제외한 해당 채널에 이미 접속한 모든사용자에게 메시지를 발송한다.
+            //socket.to('채널명').emit();
+            socket.to(channel).emit("entryOk",`${nickName}님이 ${channel} 채널에 입장했습니다.`);
+
+            //현재 채널에 입장하고 있는 사용자에게만 메시지 발송하기 
+            //socket.emit(); 현재 서버소켓을 호출한(입장하는) 사용자에게만 메시지 발송하기 
+            socket.emit("entryOk",` ${nickName} 라는 대화명으로 ${channel} 채널에 입장했습니다.`);
+        });
 
 
+        //채팅방 기준으로 해당 채팅방의 나를 포함한 모든 사용자들에게 메시지 발송하기
+        //클라이언트에서 메시지 데이터를 json포맷으로 전송한다.
+        socket.on('channelMsg',async(msgData)=>{
+            //클라이언트로 보낼 메시지 포맷 정의하기 
+            const message = `${msgData.nickName}:${msgData.message}`;
+
+            //io.to('채널명').emit()는 현재채널에
+            //메시지를 보낸 당사자(나를) 포함한 현재 채널 모든 접속자(사용자)에게 메시지 보내기
+            io.to(msgData.channel).emit('receiveChannel',message);
+        });
+
+
+
+
+
+
+        
     });
 }
 
