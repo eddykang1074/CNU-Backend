48b02940167caf0ffd624eb833f403f5a44d6973
Eddy <eddy@msoftware.co.kr>
2024-08-19 18:21:04 +0900

    백엔드 인증기능 검증 추가

--- Final/node-chat-app/routes/articleAPI.js
+++ Final/node-chat-app/routes/articleAPI.js
@@ -1,6 +1,9 @@
 var express = require("express");
 var router = express.Router();
 
+//JWT 토큰 생성을 위한 jsonwebtoken 패키지 참조하기
+const jwt = require("jsonwebtoken");
+
 //ORM db객체 참조하기
 var db = require("../models/index");
 
@@ -46,6 +49,13 @@ router.post("/create", async (req, res) => {
   };
 
   try {
+    //Step0: 프론트엔드에서 전달된 JWT토큰값에서 로그인 사용자 정보 추출하기
+    var token = req.headers.authorization.split("Bearer ")[1];
+    console.log("게시글 등록 API Token:", token);
+
+    //사용자 토큰정보 유효성 검사 후 정상적이면 토큰내에 사용자인증 json 데이터를 반환합니다.
+    var loginMember = await jwt.verify(token, process.env.JWT_AUTH_KEY);
+
     //Ste1: 프론트엔드에서 전달한 데이터 추출하기
     const title = req.body.title;
     const contents = req.body.contents;
@@ -64,7 +74,7 @@ router.post("/create", async (req, res) => {
         req.headers["x-forwarded-for"] || req.connection.remoteAddress, //로컬개발환경인 경우 ::1 이렇게 ip주소가 추출됩니다.
       is_display_code: display,
       reg_date: Date.now(),
-      reg_member_id: 1, //추후 토큰에서 사용자 정보추출하기
+      reg_member_id: loginMember.member_id, //토큰내 사용지 인증 데이터에서 사용자고유번호추출
     };
 
     //Step3: db article 테이블에 신규 게시글정보 등록처리
